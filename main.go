dd59d17c6bd0d9ef2398684090be60d4c9c574bf
Jesús Robles García <jesus.robles5186@alumnos.udg.mx>
2020-09-30 21:12:13 -0500

    epsilon ecuation

--- main.go
+++ main.go
@@ -2,6 +2,20 @@ package main
 
 import "fmt"
 
-func main()  {
-	fmt.Println("Hola Mundo!") // eliminar esta línea
+func main() {
+	var x int
+	fmt.Scan(&x)
+
+	var r float64
+	var residuo float64
+
+	r = 1
+	for i := 1; i <= x; i++ {
+		value := float64(i)
+		residuo = r * value
+		res := 1 / residuo
+		r = r +  res
+	}
+
+	fmt.Print(r)
 }
