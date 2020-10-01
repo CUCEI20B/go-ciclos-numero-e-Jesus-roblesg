67978ffa06cbb578993df72340d8dfedda015dbb
Jesús Robles García <jesus.robles5186@alumnos.udg.mx>
2020-09-30 21:18:40 -0500

    second version

--- main.go
+++ main.go
@@ -7,14 +7,9 @@ func main() {
 	fmt.Scan(&x)
 
 	var r float64
-	var residuo float64
-
 	r = 1
 	for i := 1; i <= x; i++ {
-		value := float64(i)
-		residuo = r * value
-		res := 1 / residuo
-		r = r +  res
+		r = r + 1 / (r * float64(i))
 	}
 
 	fmt.Print(r)
